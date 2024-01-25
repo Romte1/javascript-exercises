ea58680439b8d9fa234370c336bce82c1688c172
Romte1 <ivan_ni224@hotmail.com>
2024-01-25 13:36:21 -0300

    Done 5 exercises of 12 total

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNumber,lastNumber) {
+    let sum = 0;
+    if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number'){
+        return 'ERROR';
+    }
 
+    if (firstNumber <0 || lastNumber < 0){
+        return 'ERROR';
+    }
+    smallNumber = lastNumber > firstNumber ? firstNumber : lastNumber;
+    bigNumber = lastNumber > firstNumber ? lastNumber : firstNumber;
+    for (i=smallNumber; i<=bigNumber;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
