0f285433a790ecbba9c82845f8579ef87f4000ab
Romte1 <ivan_ni224@hotmail.com>
2024-01-25 12:07:32 -0300

    JS exercises. Done 2 out of 12

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(string, number) {
+    let sentence= "";
+    for (i=1; i<=number; i++){
+        sentence+=string;
+        }
+    
+    if (number<0) {
+        return 'ERROR';
+    }
+    return sentence;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
