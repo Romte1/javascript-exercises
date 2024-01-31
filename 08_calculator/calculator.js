a08a35b2d5aaaaba06ebad94b0d2f6e52cb5f213
Romte1 <ivan_ni224@hotmail.com>
2024-01-31 20:15:47 -0300

    8/12 exercises done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(a) {
+  let total = 0;
+  a.forEach((e) =>total+=e);
+  return total;
+
+};
+
+const multiply = function(a) {
+  let total = 1;
+  a.forEach((e) =>total*=e);
+  return total;
+
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b)
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+	for (i = a; i > 0; i--) {
+    factorial *=i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
